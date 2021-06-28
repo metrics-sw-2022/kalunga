d0ff52f5a191067906c7f6da214451f8f0e994ab
Jhonnatan Eduardo Zamudio Palacios <tecsco2013@gmail.com>
2021-06-27 22:21:40 -0500

    Se agregaron los recursos visuales para la pantalla SplashScreenActivity.kt

--- app/src/main/java/com/jhonnatan/kalunga/Core/Home/SplashScreen/Presentation/View/SplashScreenActivity.kt
+++ app/src/main/java/com/jhonnatan/kalunga/Core/Home/SplashScreen/Presentation/View/SplashScreenActivity.kt
@@ -4,10 +4,21 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.jhonnatan.kalunga.R
 
+/****
+ * Project: kalunga
+ * From: com.jhonnatan.kalunga.Core.Home.SplashScreen.Presentation.ViewModel
+ * Created by Jhonnatan E. Zamudio P. on 27/06/2021 at 8:59 p. m.
+ * More info:  https://venecambios-kalunga.com/
+ * All rights reserved 2021.
+ ****/
+
 class SplashScreenActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
+
+
 
     }
 }
---
+++ app/src/main/java/com/jhonnatan/kalunga/Core/Home/SplashScreen/Presentation/ViewModel/SplashScreenViewModel.kt
@@ -0,0 +1,15 @@
+package com.jhonnatan.kalunga.Core.Home.SplashScreen.Presentation.ViewModel
+
+import androidx.lifecycle.ViewModel
+
+/****
+ * Project: kalunga
+ * From: com.jhonnatan.kalunga.Core.Home.SplashScreen.Presentation.ViewModel
+ * Created by Jhonnatan E. Zamudio P. on 27/06/2021 at 8:59 p. m.
+ * More info:  https://venecambios-kalunga.com/
+ * All rights reserved 2021.
+ ****/
+
+class SplashScreenViewModel: ViewModel() {
+
+}
