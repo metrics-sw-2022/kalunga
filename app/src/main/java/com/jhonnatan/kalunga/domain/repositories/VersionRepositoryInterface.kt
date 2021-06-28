d92a1d5c6fbbb712d7c096873a2bd09f970f281c
Jhonnatan Eduardo Zamudio Palacios <tecsco2013@gmail.com>
2021-06-28 12:15:21 -0500

    Se implemento VersionRepository.kt con sus respectivas interfaces y dataSources

---
+++ app/src/main/java/com/jhonnatan/kalunga/domain/repositories/VersionRepositoryInterface.kt
@@ -0,0 +1,25 @@
+package com.jhonnatan.kalunga.domain.repositories
+
+import androidx.lifecycle.LiveData
+import com.jhonnatan.kalunga.data.source.local.entities.Version
+
+/****
+ * Project: kalunga
+ * From: com.jhonnatan.kalunga.domain.repositories
+ * Created by Jhonnatan E. Zamudio P. on 28/06/2021 at 12:06 p. m.
+ * More info:  https://venecambios-kalunga.com/
+ * All rights reserved 2021.
+ ****/
+
+interface VersionRepositoryInterface {
+
+    fun queryLast(): LiveData<Version>
+
+    suspend fun insert(version: Version)
+
+    suspend fun update(version: Version)
+
+    suspend fun delete(version: Version)
+
+    suspend fun clear()
+}
