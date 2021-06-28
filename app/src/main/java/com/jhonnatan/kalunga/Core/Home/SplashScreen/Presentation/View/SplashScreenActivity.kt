cd94843b0cbf21baf658163c8588aafd19973507
Jhonnatan Eduardo Zamudio Palacios <tecsco2013@gmail.com>
2021-06-27 20:03:14 -0500

    Se implementa la organización del feature con su respectiva arquitectura

---
+++ app/src/main/java/com/jhonnatan/kalunga/Core/Home/SplashScreen/Presentation/View/SplashScreenActivity.kt
@@ -0,0 +1,13 @@
+package com.jhonnatan.kalunga.Core.Home.SplashScreen.Presentation.View
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.jhonnatan.kalunga.R
+
+class SplashScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+    }
+}
