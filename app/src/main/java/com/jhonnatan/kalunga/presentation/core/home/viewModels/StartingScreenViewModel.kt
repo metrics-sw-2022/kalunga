bc245ba828e47b86ec811cb9cffd1c2a549542b2
Jhonnatan Eduardo Zamudio Palacios <tecsco2013@gmail.com>
2021-07-04 18:31:54 -0500

    Se implementan los elementos visuales de la pantalla de inicio

---
+++ app/src/main/java/com/jhonnatan/kalunga/presentation/core/home/viewModels/StartingScreenViewModel.kt
@@ -0,0 +1,12 @@
+package com.jhonnatan.kalunga.presentation.core.home.viewModels
+
+/****
+ * Project: kalunga
+ * From: com.jhonnatan.kalunga.presentation.core.home.viewModels
+ * Created by Jhonnatan E. Zamudio P. on 4/07/2021 at 5:35 p. m.
+ * More info:  https://venecambios-kalunga.com/
+ * All rights reserved 2021.
+ ****/
+
+class StartingScreenViewModel {
+}
