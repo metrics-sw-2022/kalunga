fc385da1060cdcabc00603254301dda592200a5d
Jhonnatan Eduardo Zamudio Palacios <tecsco2013@gmail.com>
2021-07-08 21:30:36 -0500

    Se implementa el objeto RetrofitHelper.kt para la configuración del API REST

---
+++ app/src/main/java/com/jhonnatan/kalunga/domain/common/utils/RetrofitHelper.kt
@@ -0,0 +1,21 @@
+package com.jhonnatan.kalunga.domain.common.utils
+
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+/****
+ * Project: kalunga
+ * From: com.jhonnatan.kalunga.domain.common.utils
+ * Created by Jhonnatan E. Zamudio P. on 8/07/2021 at 9:26 p. m.
+ * More info:  https://venecambios-kalunga.com/
+ * All rights reserved 2021.
+ ****/
+
+object RetrofitHelper {
+    fun getRetrofit():Retrofit {
+        return Retrofit.Builder()
+            .baseUrl("https://venecambios-kalunga.com/api/v1/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+}
