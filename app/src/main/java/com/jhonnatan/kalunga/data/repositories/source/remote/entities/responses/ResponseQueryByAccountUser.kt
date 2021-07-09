c921658ff957b3d9c5cf6069d2d950e7aff18393
Jhonnatan Eduardo Zamudio Palacios <tecsco2013@gmail.com>
2021-07-08 22:37:06 -0500

    Se configura el ResponseQueryByAccountUser.kt

---
+++ app/src/main/java/com/jhonnatan/kalunga/data/repositories/source/remote/entities/responses/ResponseQueryByAccountUser.kt
@@ -0,0 +1,18 @@
+package com.jhonnatan.kalunga.data.repositories.source.remote.entities.responses
+
+import com.google.gson.annotations.SerializedName
+import com.jhonnatan.kalunga.data.repositories.source.remote.entities.User
+
+/****
+ * Project: kalunga
+ * From: com.jhonnatan.kalunga.data.repositories.source.remote.entities.responses
+ * Created by Jhonnatan E. Zamudio P. on 8/07/2021 at 10:35 p. m.
+ * More info:  https://venecambios-kalunga.com/
+ * All rights reserved 2021.
+ ****/
+
+data class ResponseQueryByAccountUser(
+    @SerializedName("status") val status: String,
+    @SerializedName("message") val message: String,
+    @SerializedName("data") val data: User,
+)
