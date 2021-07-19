d1a27cd2b8cadec88a7f26d3d56bdec236806d9a
Laura Sofia Sarmiento <frontend@bmpcompany.com.co>
2021-07-19 17:31:46 -0500

    Caso 21: la contraseña tiene más de 5 caracteres, el método isValidLong() debe devolver true (VERDE)

--- app/src/main/java/com/jhonnatan/kalunga/domain/useCases/SignUpUseCase.kt
+++ app/src/main/java/com/jhonnatan/kalunga/domain/useCases/SignUpUseCase.kt
@@ -20,8 +20,8 @@ class SignUpUseCase {
         return text.matches(emailValidation.toRegex())
     }
 
-    fun isValidLong(text: String, min: Int) : Boolean? {
-        return null
+    fun isValidLong(text: String, min: Int) : Boolean {
+        return text.length > min
     }
 
 }
