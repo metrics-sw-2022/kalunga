3e8429a89b65bde911a328c48328c32c3b388a04
Jhonnatan Eduardo Zamudio Palacios <tecsco2013@gmail.com>
2021-07-25 14:57:27 -0500

    Se realiza animación para los tipos de documento que son más largos que el campo visual

--- app/src/main/java/com/jhonnatan/kalunga/presentation/core/session/views/ConfigurationActivity.kt
+++ app/src/main/java/com/jhonnatan/kalunga/presentation/core/session/views/ConfigurationActivity.kt
@@ -1,7 +1,9 @@
 package com.jhonnatan.kalunga.presentation.core.session.views
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
+import android.text.method.ScrollingMovementMethod
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProvider
@@ -9,11 +11,12 @@ import com.jhonnatan.kalunga.R
 import com.jhonnatan.kalunga.databinding.ActivityConfigurationBinding
 import com.jhonnatan.kalunga.domain.models.enumeration.CodeTypeSpinner
 import com.jhonnatan.kalunga.domain.models.utils.UtilsCountry
-import com.jhonnatan.kalunga.presentation.core.utils.CustomSpinnerAdapter
 import com.jhonnatan.kalunga.presentation.core.session.viewModels.ConfigurationViewModel
 import com.jhonnatan.kalunga.presentation.core.session.viewModels.ConfigurationViewModelFactory
+import com.jhonnatan.kalunga.presentation.core.utils.CustomSpinnerAdapter
 import com.jhonnatan.kalunga.presentation.core.utils.ListDialog
 import kotlinx.coroutines.DelicateCoroutinesApi
+
 
 @DelicateCoroutinesApi
 class ConfigurationActivity : AppCompatActivity() {
@@ -21,6 +24,7 @@ class ConfigurationActivity : AppCompatActivity() {
     private lateinit var binding: ActivityConfigurationBinding
     private val tag = "Configuration"
 
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val viewModelFactory = ConfigurationViewModelFactory.getInstance(this)
@@ -34,12 +38,14 @@ class ConfigurationActivity : AppCompatActivity() {
         }
 
         binding.buttonCountry.setOnClickListener {
-            createDialogSpinner(viewModel.countriesList,CodeTypeSpinner.COUNTRIES.code)
+            createDialogSpinner(viewModel.countriesList, CodeTypeSpinner.COUNTRIES.code)
         }
 
         binding.buttonDocumentType.setOnClickListener {
-            createDialogSpinner(viewModel.typeDocumentsList,CodeTypeSpinner.TYPE_DOCUMENT.code)
+            createDialogSpinner(viewModel.typeDocumentsList, CodeTypeSpinner.TYPE_DOCUMENT.code)
         }
+
+        binding.buttonDocumentType.setSelected(true)
 
         viewModel.countrySelectedPosition.observe(this, {
             if (it != null) {
@@ -49,6 +55,15 @@ class ConfigurationActivity : AppCompatActivity() {
                     R.drawable.ic_arrow_drop_down,
                     0
                 )
+            }
+        })
+
+
+
+        viewModel.typeDocumentSelectedPosition.observe(this, {
+            if (it != null) {
+                binding.buttonDocumentType.text =
+                    viewModel.typeDocumentsList[it].abbreviate + getString(R.string.separador) + viewModel.typeDocumentsList[it].description
             }
         })
 
@@ -65,8 +80,10 @@ class ConfigurationActivity : AppCompatActivity() {
             object : CustomSpinnerAdapter.CustomActionSpinner {
                 override fun onItemSelected(position: Int) {
                     when (code) {
-                        CodeTypeSpinner.COUNTRIES.code -> viewModel.countrySelectedPosition.value = position
-                        CodeTypeSpinner.TYPE_DOCUMENT.code -> viewModel.typeDocumentSelectedPosition.value = position
+                        CodeTypeSpinner.COUNTRIES.code -> viewModel.countrySelectedPosition.value =
+                            position
+                        CodeTypeSpinner.TYPE_DOCUMENT.code -> viewModel.typeDocumentSelectedPosition.value =
+                            position
                     }
                 }
             })
